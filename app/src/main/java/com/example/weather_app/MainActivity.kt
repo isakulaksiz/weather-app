15bad6e2f2eed0896907c834837def05a9047eaf
isakulaksiz <isakulaksiz@outlook.com>
2022-01-13 17:01:49 +0300

    [#1]feat: added location permission

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.weather_app
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(!isLocatinEnabled()){
+            Toast.makeText(this@MainActivity, "Your location is turned off", Toast.LENGTH_SHORT).show()
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        }else
+            Toast.makeText(this@MainActivity, "Your location provider is already TURNED ON.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isLocatinEnabled(): Boolean{
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 }
