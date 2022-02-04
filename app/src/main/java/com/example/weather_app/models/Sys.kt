0484dd411c80142a604764db1c5ce3600580fb92
isakulaksiz <isakulaksiz@outlook.com>
2022-02-04 11:41:13 +0300

    [#3] add models based on the api

---
+++ app/src/main/java/com/example/weather_app/models/Sys.kt
@@ -0,0 +1,11 @@
+package com.weatherapp.models
+
+import java.io.Serializable
+
+data class Sys(
+    val type: Int,
+    val id: Int,
+    val country: String,
+    val sunrise: Int,
+    val sunset: Int
+) : Serializable
