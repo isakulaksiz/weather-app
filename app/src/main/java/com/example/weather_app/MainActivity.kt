818d97b7f118d7d6a15e26b072dd6a51b134dc2a
isakulaksiz <isakulaksiz@outlook.com>
2022-02-06 10:30:28 +0300

    change the ui depending on api

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -216,6 +216,23 @@ class MainActivity : AppCompatActivity() {
             binding.tvSpeed.text = weatherList.wind.speed.toString()
             binding.tvName.text = weatherList.name
             binding.tvCountry.text = weatherList.sys.country
+
+            when(weatherList.weather[i].icon){
+                "01d" -> binding.ivMain.setImageResource(R.drawable.sunny)
+                "02d" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "03d" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "04d" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "04n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "10d" -> binding.ivMain.setImageResource(R.drawable.rain)
+                "11d" -> binding.ivMain.setImageResource(R.drawable.storm)
+                "13d" -> binding.ivMain.setImageResource(R.drawable.snowflake)
+                "01n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "02n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "03n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "10n" -> binding.ivMain.setImageResource(R.drawable.cloud)
+                "11n" -> binding.ivMain.setImageResource(R.drawable.rain)
+                "13n" -> binding.ivMain.setImageResource(R.drawable.snowflake)
+            }
         }
     }
 
